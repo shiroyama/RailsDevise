f840d03eea41df200ec3dbf1e22a3dab14f2072f
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-02-28 06:35:25 +0000

    Add test print in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+      p "user_signed_in?: "
+      p user_signed_in?
+
+      p "current_user: "
+      p current_user
+
+      p "user_session: "
+      if !user_session.nil?
+        user_session[:fuck] ||= 0
+        user_session[:fuck] += 1
+      end
+      p user_session
   end
 end
